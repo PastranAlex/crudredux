1336dfd5e1394aeb64d971a87fe831c6b7c125de
root <root@DESKTOP-1VF8D98.localdomain>
2021-02-02 00:13:05 -0300

    ReduxReact: primera seccion

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import Header from './components/Header';
 import Productos from './components/Productos';
 import NuevoProducto from './components/NuevoProducto';
@@ -6,18 +6,25 @@ import EditarProducto from './components/EditarProducto';
 
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
+
+//redux
+import { Provider } from 'react-redux';
+import store from './store';
+
 function App() {
   return (
     <Router>
-      <Header />
+      <Provider store={store}>
+        <Header />
 
-      <div className="container mt-5">
-        <Switch>
-          <Route exact path="/" component={Productos} />
-          <Route exact path="/productos/nuevo" component={NuevoProducto} />
-          <Route exact path="/productos/editar/:id" component={EditarProducto} />
-        </Switch>
-      </div>
+        <div className="container mt-5">
+          <Switch>
+            <Route exact path="/" component={Productos} />
+            <Route exact path="/productos/nuevo" component={NuevoProducto} />
+            <Route exact path="/productos/editar/:id" component={EditarProducto} />
+          </Switch>
+        </div>
+      </Provider>
     </Router>
   );
 }
--- src/components/NuevoProducto.jsx
+++ src/components/NuevoProducto.jsx
@@ -1,8 +1,73 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+// actions de redux
+import { crearNuevoProductoAction } from '../actions/productoActions'
+import { useDispatch, useSelector } from 'react-redux';
+
 
 const NuevoProducto = () => {
+  
+  const [nombre, guardarNombre] = useState('');
+  const [precio, guardarPrecio] = useState(0);
+
+  const dispach = useDispatch();
+
+  const agregarProducto = producto => dispach( crearNuevoProductoAction(producto) )
+
+  const submitNuevoProducto = e => {
+    e.preventDefault();
+
+    if ( nombre.trim() === '' || precio <= 0 ) {
+      return;
+    }
+
+    agregarProducto({nombre, precio});
+  }
+  
   return ( 
-    <h1>Desde NuevoProducto.js</h1>
+    <div className="row justify-content-center">
+      <div className="col-md-8">
+        <div className="card">
+          <div className="card-body">
+            <h2 className="text-center mb-4 font-weight-bold">
+              Agregar nuevo producto
+            </h2>
+
+            <form
+              onSubmit={submitNuevoProducto}
+            >
+              <div className="form-group">
+                <label>Nombre Producto</label>
+                <input 
+                  type="text"
+                  className="form-control"
+                  placeholder="Nombre Producto"
+                  name="nombre"
+                  value={nombre}
+                  onChange={e => guardarNombre(e.target.value)}
+                />
+              </div>
+              <div className="form-group">
+                <label>Precio Producto</label>
+                <input 
+                  type="number"
+                  className="form-control"
+                  placeholder="Precio Producto"
+                  name="precio"
+                  value={precio}
+                  onChange={e => guardarPrecio(Number(e.target.value))}
+                />
+              </div>
+
+              <button
+                type="submit"
+                className="btn btn-primary font-weight-bold text-uppercase d-block w-100"
+              >Agregar</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
    );
 }
  
